87455e4a27439e9f4e1a7181e6e7ba3c205e15e7
Taras Moskovych <moskovych.taras@gmail.com>
2019-12-04 23:25:17 +0200

    add extra styles, extend functionality

--- tooltip.js
+++ tooltip.js
@@ -3,40 +3,54 @@ class Tooltip extends HTMLElement {
     super();
 
     this._container = null;
-    this._tooltip = null;
+    this._tooltipIcon = null;
     this.attachShadow({ mode: 'open' });
   }
 
+  // listen to attributes changing
+  static get observedAttributes() {
+    return ['text'];
+  }
+
   connectedCallback() {
-    this._initVariables();
     this._initUI();
     this._initListeners();
   }
 
-  _initVariables() {
+  disconnectedCallback() {
+    this._toggleListeners('removeEventListener');
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) { return; }
+
+    if (name === 'text') {
+      this._text = newValue;
+      this._tooltipIcon && this._tooltipIcon.setAttribute('aria-label', newValue);
+    }
+  }
+
+  _initUI() {
     this._text = this.getAttribute('text') || 'Default tooltip text';
     this._icon = this.getAttribute('icon') || ' (?)';
     this._position = this.getAttribute('position') || 'bottom';
 
     this.shadowRoot.innerHTML = `
       <style>
-        span {
-          cursor: pointer;
-        }
-
-        div {
+        .tooltip {
           background-color: #555;
           border-radius: 10px;
+          box-shadow: 1px 1px 6px rgba(0,0,0,0.5);
           color: white;
           font-size: .85em;
+          font-weight: normal;
           padding: 5px;
           position: absolute;
           text-align: center;
-          transition: all 1s;
           z-index: 10;
         }
 
-        div::after {
+        .tooltip::after {
           border-style: solid;
           border-width: 5px;
           content: "";
@@ -56,44 +70,89 @@ class Tooltip extends HTMLElement {
           right: 100%;
           top: 50%;
         }
+
+        .icon {
+          background: #555;
+          border-radius: 50%;
+          color: white;
+          cursor: pointer;
+          padding: .05rem .4rem;
+          text-align: center;
+        }
+
+        .icon:hover,
+        .icon:focus {
+          background: #000;
+        }
+
+        :host {
+          position: relative;
+        }
+
+        :host(.important) {
+          color: var(--color-primary, blue);
+        }
+
+        :host-context(p.parent) {
+          color: blue;
+        }
+
+        ::slotted(.highlight) {
+          border-bottom: 1px solid red;
+        }
       </style>
 
       <slot>Default value</slot>
-      <span>${this._icon}</span>
+      <span tabindex="0" aria-label="${this._text}" class="icon">${this._icon}</span>
     `;
 
-    this._tooltip = this.shadowRoot.querySelector('span');
-  }
-
-  _initUI() {
-    this.shadowRoot.appendChild(this._tooltip);
-
-    this.style.position = 'relative';
+    this._tooltipIcon = this.shadowRoot.querySelector('.icon');
   }
 
   _initListeners() {
-    this._tooltip.addEventListener('mouseenter', this._show.bind(this));
-    this._tooltip.addEventListener('mouseleave', this._hide.bind(this));
+    this._toggleListeners('addEventListener');
   }
 
   _show() {
+    if (this._contains()) { return; }
+
     this._container = document.createElement('div');
     this._container.textContent = this._text;
-    this._container.classList.add(this._position);
+    this._container.classList.add(this._position, 'tooltip');
 
     this.shadowRoot.appendChild(this._container);
     this._setPosition();
   }
 
   _hide() {
-    this.shadowRoot.removeChild(this._container);
+    if (this._contains()) {
+      this.shadowRoot.removeChild(this._container);
+    }
+  }
+
+  _contains() {
+    return this.shadowRoot.contains(this._container);
   }
 
   _setPosition() {
-    const tooltipRect = this._tooltip.getBoundingClientRect();
+    const tooltipRect = this._tooltipIcon.getBoundingClientRect();
 
-    this._container.style.top = this._position === 'bottom' ? `${tooltipRect.height + 10}px` : `${-tooltipRect.height / 2 - 10}px`;
-    this._container.style.left = this._position === 'bottom' ? `${tooltipRect.left - this._container.getBoundingClientRect().width / 4 - 5}px` : `${tooltipRect.right}px`;
+    this._container.style.top = this._position === 'bottom'
+      ? `${tooltipRect.height + 10}px`
+      : `${-tooltipRect.height / 2 - 10}px`;
+
+    this._container.style.left = this._position === 'bottom'
+      ? `${tooltipRect.left - this._container.getBoundingClientRect().width / 4 - 5}px`
+      : `${tooltipRect.right}px`;
+  }
+
+  _toggleListeners(method) {
+    if (typeof this._tooltipIcon[method] !== 'function') { throw new Error(`Wrong method name: "${method}"`); }
+
+    this._tooltipIcon[method]('mouseenter', this._show.bind(this));
+    this._tooltipIcon[method]('mouseleave', this._hide.bind(this));
+    this._tooltipIcon[method]('focusin', this._show.bind(this));
+    this._tooltipIcon[method]('focusout', this._hide.bind(this));
   }
 }
 
